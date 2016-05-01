ad432dc9cab16d8b1849641b3ccd5132adfea5da
darnell1894 <mdarnell@lions.molloy.edu>
2016-05-01 15:30:55 -0400

    Create ttt-model.js

---
+++ ttt-model.js
@@ -0,0 +1,138 @@
+"use strict";
+
+function Model(row,column)
+{
+	var board = [];
+	this.board = board;
+	this.row = row;
+	this.column = column;
+	this.player = [];
+	this.player[0] = 'X';
+	this.player[1] = 'O';
+	this.moves = 0;
+	this.turn = this.player[0];
+}
+
+var model = {
+
+	constructor: Model,
+
+	newGame: function(){
+		this.moves = 0;
+		for(var i = 0; i < this.row; i++)
+		{
+			this.board[i] = [];
+			for(var j = 0; j < this.column; j++)
+			{
+				this.board[i][j] = '';
+			}
+		}
+	},
+	
+	makeMove: function(r,c){
+		
+		if(this.isValidMove(r,c))
+		{
+			this.board[r][c] = this.turn;
+			this.moves++;
+			this.turn = this.player[this.moves%2];
+		}
+		else
+		{
+			console.log("Invalid Move");
+		}
+	},
+	
+	isValidMove: function(r,c){
+		
+		if(this.board[r][c] === '')
+		{
+			return true;
+		}
+		return false;
+	},
+	
+	isDraw: function(){
+
+		for(var i = 0; i < this.row; i++)
+		{
+			for(var j = 0; j <this.column; j++)
+			{
+				if(this.board[i][j] === '')
+				{
+					return false;
+				}
+			}
+		}
+	
+		return true;
+	},
+	
+	checkWin: function(){
+		
+		if((this.board[0][0] === this.board[1][1] && this.board[0][0] === this.board[2][2]) || (this.board[0][2] === this.board[1][1] && this.board[0][2] === this.board[2][0]))
+		{
+			console.log(this.getSquare(1,1)+" wins diagonal");
+			return this.getSquare(1,1);
+		} // diagonal test
+		
+		for (var ii = 0; ii < this.row; ii++)
+		{
+			if(this.board[ii][0] === this.board[ii][1] && this.board[ii][1] === this.board[ii][2])
+			{
+				console.log(this.getSquare(ii,0)+" wins horizontal");
+				return this.getSquare(ii,0);
+			}
+		} // horizontal test
+		
+		for (var jj = 0; jj < this.column; jj++)
+		{
+			if(this.board[0][jj] === this.board[1][jj] && this.board[0][jj] === this.board[2][jj])
+			{
+				console.log(this.getSquare(0,jj)+" wins vertical");
+				return this.getSquare(0,jj);
+			}
+		} // vertical test
+		
+		if(this.isDraw())
+		{
+			console.log("draw");
+			return "draw";
+		}
+		console.log("no winner");
+		return false; // no winner or draw
+	},
+	
+	
+	
+	getSquare: function(r, c) {
+		return (this.board[r][c]);
+	},
+
+	copy: function(){
+		
+		var copy = new Model(this.row, this.column);
+		copy.newGame();
+		copy.moves = this.moves;
+		copy.turn = this.turn;
+		
+		for(var i in this.board)
+		{
+			copy.board[i] = this.board[i].slice();
+		} 
+		
+		return copy;
+	},
+	
+	printBoard: function(){
+		
+		for(var i in this.board)
+		{
+			console.log(this.board[i]);
+		}
+		
+	}
+
+};
+
+Model.prototype = model;
